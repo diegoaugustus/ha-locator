acfaef6028668569983ac3bb88a49d9bc0e3a670
diegoaugustus <diegoaugustus@gmail.com>
2025-05-17 18:28:00 -0300

    Update run.sh

--- ha-locator/run.sh
+++ ha-locator/run.sh
@@ -3,17 +3,22 @@
 echo "Iniciando ha-locator"
 sleep 1
 
-MQTT_BROKER=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
-TOPIC="meu/topico"
-
-echo "Iniciando subscription no tópico: $TOPIC"
-
-mosquitto_sub -h "$MQTT_BROKER" -u "$MQTT_USER" -P "$MQTT_PASSWORD" -t "$TOPIC" | while read -r line; do
-  echo "Nova mensagem recebida:"
-  echo "$line" | jq .
+while true; do
+  echo "Hello world!"
+  sleep 1
 done
+
+# MQTT_BROKER=$(bashio::services mqtt "host")
+# MQTT_USER=$(bashio::services mqtt "username")
+# MQTT_PASSWORD=$(bashio::services mqtt "password")
+# TOPIC="meu/topico"
+
+# echo "Iniciando subscription no tópico: $TOPIC"
+
+# mosquitto_sub -h "$MQTT_BROKER" -u "$MQTT_USER" -P "$MQTT_PASSWORD" -t "$TOPIC" | while read -r line; do
+#   echo "Nova mensagem recebida:"
+#   echo "$line" | jq .
+# done
 
 # Mantém o script ativo
 tail -f /dev/null
